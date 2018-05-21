77aaabed6dea82f8441b4b83b15dc4d53fe516bc
guangtoutou <ni.ningning@gmail.com>
2018-05-21 23:29:47 +0800

    add a login form with inline error

---
+++ ui/src/components/forms/LoginForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
+import PropTypes from 'prop-types';
+
+class LoginForm extends Component {
+  state = {
+    data: {
+      username: '',
+      password: ''
+    },
+    loading: false,
+    errors: {}
+  };
+
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) this.props.submit(this.state.data);
+  };
+
+  validate = data => {
+    const errors = {};
+    if (Validator.isEmpty(data.username))
+      errors.username = "Username can't be empty";
+    if (Validator.isEmpty(data.password))
+      errors.password = "Password can't be empty";
+    return errors;
+  };
+
+  render() {
+    const { data, errors } = this.state;
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field error={!!errors.username}>
+          <label htmlFor="username">User Name</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="James"
+            value={data.username}
+            onChange={this.onChange}
+          />
+          {errors.username && <InlineError text={errors.username} />}
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Make it secure"
+            value={data.password}
+            onChange={this.onChange}
+          />
+          {errors.password && <InlineError text={errors.password} />}
+        </Form.Field>
+        <Button primary>Login</Button>
+      </Form>
+    );
+  }
+}
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
+
+export default LoginForm;
