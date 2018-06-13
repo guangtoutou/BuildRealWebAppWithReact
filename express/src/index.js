ef275fa99d24284655df9a31be9736fb7209357c
guangtoutou <ni.ningning@gmail.com>
2018-06-13 23:22:21 +0800

    setup eslint, prettier

--- express/src/index.js
+++ express/src/index.js
@@ -1,17 +1,16 @@
-import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import express from 'express';
 
 const app = express();
-
 app.use(cors());
 
-app.post("/login", (req, res)=>{
-      res.status(400).json("error")
-})
+app.post('/login', (req, res) => {
+  res.status(400).json('error');
+});
 
-app.get("/", (req,res)=>{
-  res.sendFile(path.join(__dirname,'index.html'));
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.listen(8080,()=> console.log("running on localhost:8080"));
+app.listen(8080, () => console.log('running on localhost:8080'));
